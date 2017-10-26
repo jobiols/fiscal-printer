2bdbfcf16f55f5fd1e66b31e1fc4fefcf66ccdd5
jobiols <jorge.obiols@gmail.com>
2017-10-25 21:14:13 -0300

    [REF]  reformatear epson.py

--- fpoc/models/epson.py
+++ fpoc/models/epson.py
@@ -11,12 +11,14 @@ from controllers.main import do_event
 import logging
 
 _logger = logging.getLogger(__name__)
-#_logger.setLevel(logging.DEBUG)
+# _logger.setLevel(logging.DEBUG)
 
 _schema = logging.getLogger(__name__ + '.schema')
 
-_header_lines = ['headerLine 1', 'headerLine 2', 'headerLine 3', 'headerLine 4', 'headerLine 5', 'headerLine 6', 'headerLine 7']
-_footer_lines = ['footerLine 1', 'footerLine 2', 'footerLine 3', 'footerLine 4', 'footerLine 5', 'footerLine 6', 'footerLine 7']
+_header_lines = ['headerLine 1', 'headerLine 2', 'headerLine 3', 'headerLine 4', 'headerLine 5', 'headerLine 6',
+                 'headerLine 7']
+_footer_lines = ['footerLine 1', 'footerLine 2', 'footerLine 3', 'footerLine 4', 'footerLine 5', 'footerLine 6',
+                 'footerLine 7']
 
 
 class epson_ar_fiscal_printer(osv.osv):
@@ -29,34 +31,34 @@ class epson_ar_fiscal_printer(osv.osv):
     def _get_field(self, cr, uid, ids, field_name, args, context):
         r = {}
         for fp in self.browse(cr, uid, ids):
-            r[fp.id] = { fn: False for fn in field_name }
+            r[fp.id] = {fn: False for fn in field_name}
             event_result = do_event('read_attributes', {'name': fp.name},
-                     session_id=fp.session_id, printer_id=fp.name)
+                                    session_id=fp.session_id, printer_id=fp.name)
             event_result = event_result.pop() if event_result else {}
             if event_result and 'attributes' in event_result:
                 attrs = event_result['attributes']
-                r[fp.id]['header'] = '\n'.join([ attrs[k] for k in _header_lines
-                                                if k in attrs and attrs[k] ])
-                r[fp.id]['footer'] = '\n'.join([ attrs[k] for k in _footer_lines
-                                                if k in attrs and attrs[k] ])
+                r[fp.id]['header'] = '\n'.join([attrs[k] for k in _header_lines
+                                                if k in attrs and attrs[k]])
+                r[fp.id]['footer'] = '\n'.join([attrs[k] for k in _footer_lines
+                                                if k in attrs and attrs[k]])
                 for fn in field_name:
                     if fn in attrs:
                         if fn in ['tasaIVA', 'maxMonto']:
                             try:
-                                r[fp.id][fn] = float(attrs[fn])/100.
+                                r[fp.id][fn] = float(attrs[fn]) / 100.
                             except:
                                 r[fp.id][fn] = 0
                         elif fn in ['fechaFiscalizacion']:
                             line = attrs[fn]
-                            r[fp.id][fn] = "20{2}-{1}-{0}".format(*[line[i:i+2] for i in range(0, len(line), 2)])
+                            r[fp.id][fn] = "20{2}-{1}-{0}".format(*[line[i:i + 2] for i in range(0, len(line), 2)])
                         else:
                             r[fp.id][fn] = attrs[fn]
         return r
 
     def _put_field(self, cr, uid, ids, field_name, field_value, arg, context):
         fp = self.browse(cr, uid, ids)
-        data = { 'name': fp.name,
-                 'attributes': {} }
+        data = {'name': fp.name,
+                'attributes': {}}
         if (field_name == 'header'):
             lines = field_value.split('\n')[:len(_header_lines)] if field_value else []
             lines = lines + (len(_header_lines) - len(lines)) * ['']
@@ -66,28 +68,41 @@ class epson_ar_fiscal_printer(osv.osv):
             lines = lines + (len(_footer_lines) - len(lines)) * ['']
             data['attributes'].update(dict(zip(_footer_lines, lines)))
         event_result = do_event('write_attributes', data,
-                 session_id=fp.session_id, printer_id=fp.name)
+                                session_id=fp.session_id, printer_id=fp.name)
         return True
 
     _columns = {
-        'header': fields.function(_get_field, fnct_inv=_put_field, type="text", method=True, multi='epson_text', store=False, string='Header'),
-        'footer': fields.function(_get_field, fnct_inv=_put_field, type="text", method=True, multi='epson_text', store=False, string='Footer'),
+        'header': fields.function(_get_field, fnct_inv=_put_field, type="text", method=True, multi='epson_text',
+                                  store=False, string='Header'),
+        'footer': fields.function(_get_field, fnct_inv=_put_field, type="text", method=True, multi='epson_text',
+                                  store=False, string='Footer'),
 
-        'razonSocial': fields.function(_get_field, type="char", method=True, multi='epson_info', store=False, string='Razon Social'),
+        'razonSocial': fields.function(_get_field, type="char", method=True, multi='epson_info', store=False,
+                                       string='Razon Social'),
         'cuit': fields.function(_get_field, type="char", method=True, multi='epson_info', store=False, string='CUIT'),
-        'caja': fields.function(_get_field, type="char", method=True, multi='epson_info', store=False, string='Caja/Punto de Venta'),
-        'ivaResponsabilidad': fields.function(_get_field, type="char", method=True, multi='epson_info', store=False, string='Resp. IVA'),
+        'caja': fields.function(_get_field, type="char", method=True, multi='epson_info', store=False,
+                                string='Caja/Punto de Venta'),
+        'ivaResponsabilidad': fields.function(_get_field, type="char", method=True, multi='epson_info', store=False,
+                                              string='Resp. IVA'),
         'calle': fields.function(_get_field, type="char", method=True, multi='epson_info', store=False, string='Calle'),
-        'numero': fields.function(_get_field, type="char", method=True, multi='epson_info', store=False, string='Numero'),
+        'numero': fields.function(_get_field, type="char", method=True, multi='epson_info', store=False,
+                                  string='Numero'),
         'piso': fields.function(_get_field, type="char", method=True, multi='epson_info', store=False, string='Piso'),
         'depto': fields.function(_get_field, type="char", method=True, multi='epson_info', store=False, string='Depto'),
-        'localidad': fields.function(_get_field, type="char", method=True, multi='epson_info', store=False, string='Localidad'),
-        'cpa': fields.function(_get_field, type="char", method=True, multi='epson_info', store=False, string='Cod.Pos.'),
-        'provincia': fields.function(_get_field, type="char", method=True, multi='epson_info', store=False, string='Provincia'),
-        'tasaIVA': fields.function(_get_field, type="float", method=True, multi='epson_info', store=False, string='Tasa IVA'),
-        'maxMonto': fields.function(_get_field, type="float", method=True, multi='epson_info', store=False, string='Monto Maximo'),
-        'fechaFiscalizacion': fields.function(_get_field, type="date", method=True, multi='epson_info', store=False, string='Fecha Fiscalizacion'),
+        'localidad': fields.function(_get_field, type="char", method=True, multi='epson_info', store=False,
+                                     string='Localidad'),
+        'cpa': fields.function(_get_field, type="char", method=True, multi='epson_info', store=False,
+                               string='Cod.Pos.'),
+        'provincia': fields.function(_get_field, type="char", method=True, multi='epson_info', store=False,
+                                     string='Provincia'),
+        'tasaIVA': fields.function(_get_field, type="float", method=True, multi='epson_info', store=False,
+                                   string='Tasa IVA'),
+        'maxMonto': fields.function(_get_field, type="float", method=True, multi='epson_info', store=False,
+                                    string='Monto Maximo'),
+        'fechaFiscalizacion': fields.function(_get_field, type="date", method=True, multi='epson_info', store=False,
+                                              string='Fecha Fiscalizacion'),
     }
+
 
 epson_ar_fiscal_printer()
 
@@ -101,19 +116,19 @@ class epson_ar_fiscal_tf_printer_configuration(osv.osv):
     _description = 'Configuracion de TF/TND para Epson Argentina'
 
     epson_type_paper_status = {
-        'epson_ar_receipt': ( 'receiptState', {
+        'epson_ar_receipt': ('receiptState', {
             0: 'ok',
             1: 'low',
             2: 'none',
             3: 'unknown',
         }),
-        'epson_ar_journal': ( 'journalState', {
+        'epson_ar_journal': ('journalState', {
             0: 'ok',
             1: 'low',
             2: 'none',
             3: 'unknown',
         }),
-        'epson_ar_slip': ( 'slipHasPaper', {
+        'epson_ar_slip': ('slipHasPaper', {
             0: 'ok',
             1: 'low',
             2: 'none',
@@ -132,17 +147,17 @@ class epson_ar_fiscal_tf_printer_configuration(osv.osv):
                     continue
                 if 'paper_state' not in stat:
                     key, rule = self.epson_type_paper_status.get(conf.type, (False, False))
-                    stat['paper_state'] = rule.get(stat.get(key, 'unknown'),'unknown')
+                    stat['paper_state'] = rule.get(stat.get(key, 'unknown'), 'unknown')
                 if 'fiscal_state' not in stat:
                     stat['fiscal_state'] = 'open' if stat['inFiscalJournal'] else 'close'
                 if 'printer_state' not in stat:
-                    stat['printer_state'] = [ v for v in ['deviceopen' if stat['isPrinterOpen']      else False,
-                                                          'onerror'    if stat['inError']            else False,
-                                                          'offline'    if stat['isOffline']          else False,
-                                                          'nomemory'   if stat['memStatus']          else False,
-                                                          'nopaper'    if stat['slipHasPaper']       else False,
-                                                          'printing'   if stat['documentInProgress'] else False,
-                                                          'ready'] if v ][0]
+                    stat['printer_state'] = [v for v in ['deviceopen' if stat['isPrinterOpen']      else False,
+                                                         'onerror' if stat['inError']            else False,
+                                                         'offline' if stat['isOffline']          else False,
+                                                         'nomemory' if stat['memStatus']          else False,
+                                                         'nopaper' if stat['slipHasPaper']       else False,
+                                                         'printing' if stat['documentInProgress'] else False,
+                                                         'ready'] if v][0]
         return r
 
     def _get_type(self, cr, uid, context=None):
@@ -150,7 +165,7 @@ class epson_ar_fiscal_tf_printer_configuration(osv.osv):
         return r + [
             ('epson_ar_receipt', _('Receipt Epson Arg.')),
             ('epson_ar_journal', _('Journal Epson Arg.')),
-            ('epson_ar_slip',    _('Slip station Epson Arg.')),
+            ('epson_ar_slip', _('Slip station Epson Arg.')),
         ]
 
     _columns = {
@@ -160,10 +175,10 @@ class epson_ar_fiscal_tf_printer_configuration(osv.osv):
         'keep_description_attributes': fields.boolean('Conservar atributos de impresion de las descripciones'),
         'store_extra_description': fields.boolean('Almacenar solo primer descripcion extra'),
         'cut_paper': fields.boolean('Cortar papel'),
-        'electronic_answer':fields.boolean('Devuelve respuesta electronica'),
-        'print_return_attribute':fields.boolean('Imprime "Su Vuelto" con atributos'),
-        'current_account_automatic_pay':fields.boolean('Utiliza pago automatico como cuenta corriente'),
-        'print_quantities':fields.boolean('Imprimir Cantidad de unidades'),
+        'electronic_answer': fields.boolean('Devuelve respuesta electronica'),
+        'print_return_attribute': fields.boolean('Imprime "Su Vuelto" con atributos'),
+        'current_account_automatic_pay': fields.boolean('Utiliza pago automatico como cuenta corriente'),
+        'print_quantities': fields.boolean('Imprimir Cantidad de unidades'),
         'tail': fields.text('Modificaciones en el pie del ticket'),
     }
 
@@ -184,10 +199,8 @@ class epson_ar_fiscal_tf_printer_configuration(osv.osv):
                 del conf['name']
                 del conf['protocol']
                 del conf['id']
-                del conf['tail'] # Proceso especial.
+                del conf['tail']  # Proceso especial.
                 r[conf_id] = conf
         return r
-
-
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
