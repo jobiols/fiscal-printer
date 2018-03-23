267c208e3e599d7c5f7e1a2e5def0649f7f59b60
jobiols <jorge.obiols@gmail.com>
2018-03-23 19:45:13 -0300

    [FIX] travis aeroolib version

---
+++ partner_fiscal_constraints/README.rst
@@ -0,0 +1,12 @@
+Partner Fiscal Constraints
+==========================
+
+Este modulo provee una serie de constraints para asegurar los datos
+que van a los controladores fiscales.
+
+- Responsable inscripto o monotributo requiere CUIT y direccion
+- El CUIT o el DNI tienen que ser numericos
+- El DNI no puede repetirse
+- El CUIT no puede repetirse
+- Consumidor final requiere DNI y direccion
+- Consumidor final anonimo requiere tipo "Sin identificar/venta global diaria"
--- partner_fiscal_constraints/__openerp__.py
+++ partner_fiscal_constraints/__openerp__.py
@@ -23,23 +23,7 @@
     'version': '8.0.1.0',
     'category': 'Support',
     'sequence': 15,
-    'summary': '',
-    'description': """
-Partner Fiscal Constraints
-==========================
-
-Este modulo provee una serie de constraints para asegurar los datos
-que van a los controladores fiscales.
-
-- Responsable inscripto o monotributo requiere CUIT y direccion
-- El CUIT o el DNI tienen que ser numericos
-- El DNI no puede repetirse
-- El CUIT no puede repetirse
-- Consumidor final requiere DNI y direccion
-- Consumidor final anonimo requiere tipo "Sin identificar/venta global diaria"
-
-
-    """,
+    'summary': 'Asegurar los datos que van a los controladores fiscales',
     'author':  'jeo Soft',
     'website': 'jeosoft.com.ar',
     'license': 'AGPL-3',
